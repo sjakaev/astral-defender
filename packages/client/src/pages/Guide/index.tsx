08bcac9de6205f0c483347a54d43afe131c39596
Роман Сякаев <r.sjakaev@reg.ru>
2024-10-03 16:49:28 +0400

    Update guide

--- packages/client/src/pages/Guide/index.tsx
+++ packages/client/src/pages/Guide/index.tsx
@@ -29,26 +29,20 @@ const Guide = () => {
           <li>
             <img src="images/bug2.png" alt="" />
             <span>2 lvl</span>
-            <span>8 hp</span>
+            <span>6 hp</span>
             <span>2 min</span>
           </li>
           <li>
             <img src="images/bug3.png" alt="" />
             <span>3 lvl</span>
-            <span>20 hp</span>
-            <span>5 min</span>
-          </li>
-          <li>
-            <img src="images/bug4.png" alt="" />
-            <span>4 lvl</span>
-            <span>50 hp</span>
-            <span>8 min</span>
+            <span>10 hp</span>
+            <span>4 min</span>
           </li>
           <li>
             <img src="images/bug5.png" alt="" />
-            <span>5 lvl</span>
-            <span>100 hp</span>
-            <span>10 min</span>
+            <span>4 lvl</span>
+            <span>14 hp</span>
+            <span>6 min</span>
           </li>
         </ul>
       </div>
