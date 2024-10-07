d5ea8bb295aec42b23b5b4c10a7bc8cfb0785358
Роман Сякаев <r.sjakaev@reg.ru>
2024-10-07 18:03:50 +0400

    Removed unnecessary code

--- packages/client/src/helpers/getAvatarSrc.ts
+++
@@ -1,7 +0,0 @@
-import { RESOURCES_URL } from '../constants'
-
-const getAvatarSrc = (avatar?: string) => {
-  return avatar ? `${RESOURCES_URL}${avatar}` : undefined
-}
-
-export default getAvatarSrc
